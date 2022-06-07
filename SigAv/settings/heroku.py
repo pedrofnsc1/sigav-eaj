47d4e688023efdefbc326451302105fccabfb93f
Pedro Ricardo Alves Fonseca <pedro.ricardo575@gmail.com>
2022-06-07 12:11:10 -0300

    adicionar arquivos para deploy com heroku

--- SigAv/settings/heroku.py
+++ SigAv/settings/heroku.py
@@ -5,8 +5,11 @@ from SigAv.settings.base import *
 env = environ.Env()
 
 DEBUG = env.bool("DEBUG", False)
+
 SECRET_KEY = env("SECRET_KEY")
+
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
 DATABASES = {
     "default": env.db(),
 }
